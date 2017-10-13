cecd6b66842784355d6d51b013c331625b84bf86
NIIKURA Shozo <niikura@nyam.co.jp>
2017-10-13 05:10:05 +0000

    cycle2 準備

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,6 +1,11 @@
 class Bowling
     def initialize
+        # 全体の点数の合計値
         @total_score = 0
+        # 全体の（フレームの）スコアを格納する配列
+        @scores = []
+        # 一時保管用の配列（現在のフレーム）
+        @temp = []
     end
     
     def add_score(pins)
@@ -10,5 +15,9 @@ class Bowling
     # スコアの合計値を返却する
     def total_score
         @total_score
+    end
+    
+    # 各（フレームの）スコアを合計する
+    def calc_score
     end
 end
