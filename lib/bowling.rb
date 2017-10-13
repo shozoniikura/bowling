b0c34d274326f874e7bc702b40b19f7d6c98a24b
NIIKURA Shozo <niikura@nyam.co.jp>
2017-10-13 05:52:45 +0000

    cycle2 test passed

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -9,7 +9,12 @@ class Bowling
     end
     
     def add_score(pins)
-       @total_score += pins
+        @temp << pins
+        if @temp.size == 2
+            @scores << @temp
+            @temp = []
+        end
+        # @total_score += pins
     end
     
     # スコアの合計値を返却する
@@ -19,5 +24,29 @@ class Bowling
     
     # 各（フレームの）スコアを合計する
     def calc_score
+        @scores.each.with_index(1) do |score, index|
+            @total_score += score.inject(:+)
+            if is_spare?(score)
+                @total_score += calc_spare_bounus(index)
+            end
+        end
+    end
+    
+    private
+    def is_spare?(score)
+        score.inject(:+) == 10
+    end
+    
+    # 最終フレームかどうかをチェックする
+    def not_last_frame?(index)
+        index < 10
+    end
+    
+    def calc_spare_bounus(index)
+       if @scores[index]
+           @scores[index].first
+       else
+           @temp.first
+       end
     end
 end
